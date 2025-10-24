dc0dcc779070ce07f197e0b985e2aaf93adec6f2
coedataanalytics <floridatechcoedataanalytics@gmail.com>
2025-10-24 14:00:06 -0400

    Add files via upload

---
+++ Temp_Test_Script.R
@@ -0,0 +1,7 @@
+if (!require(pacman)) {
+  install.packages("pacman")
+}
+
+usethis::use_git_ignore(c(".Rhistory",".RData",".Rproj.user",".Renviron",".env",
+                          "data-raw/","outputs/","logs/", "processed_data/"))
+usethis::use_readme_rmd()
