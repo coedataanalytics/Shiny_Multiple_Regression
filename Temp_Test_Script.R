4d96f4bc04793b8dcc2fa3ba96b6ac9154fdf48e
Kyle Canady <KylemCanady1@gmail.com>
2025-10-24 15:12:51 -0400

    Test_update 2

--- Temp_Test_Script.R
+++ Temp_Test_Script.R
@@ -5,3 +5,5 @@ if (!require(pacman)) {
 usethis::use_git_ignore(c(".Rhistory",".RData",".Rproj.user",".Renviron",".env",
                           "data-raw/","outputs/","logs/", "processed_data/"))
 usethis::use_readme_rmd()
+
+# testing this stuff
